0c5585a30ffa77de7bb0f10eb574bb3351e2db7c
Korkoe27 <dumashiekorkoe@gmail.com>
2025-01-30 13:22:22 +0000

    fix(ui): adjust flex direction for big and small screens

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,18 +2,18 @@ import {aboutImage2} from "../assets/index"
 import { Card } from "./Card"
 const About = () => {
   return (
-    <div className="w-full flex flex-col gap-4 px-4 lg:px-0" id="about">
+    <div className="w-full flex flex-col gap-4 lg:px-0" id="about">
       <section className="flex flex-wrap justify-evenly lg:flex-row gap-6">
-        <div className="lg:w-1/3 w-full py-6 lg:space-y-4">
+        <div className="lg:w-1/3 w-full p-10 lg:space-y-4">
           <h1 className="font-bold text-2xl">About the Project</h1>
           <p className="font-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia explicabo, id fugit maxime reiciendis assumenda in accusamus rem minima? Mollitia sapiente, natus illo ut repudiandae sunt ex nemo nihil culpa?</p>
         </div>
 
-        <Card image_path={aboutImage2} direction={'normal'} title={"Focus"} description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, esse."/>
+        <Card image_path={aboutImage2} direction={'reverse'} title={"Focus"} description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, esse."/>
         <Card image_path={aboutImage2} direction={'reverse'} title={"Focus"} description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, esse."/>
         <Card image_path={aboutImage2} direction={'normal'} title={"Focus"} description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, esse."/>
-        <Card image_path={aboutImage2} title={"Focus"} direction={'reverse'} description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, esse."/>
-        <Card image_path={aboutImage2} direction={'normal'} title={"Focus"} description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, esse."/>
+        <Card image_path={aboutImage2} title={"Focus"} direction={'normal'} description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, esse."/>
+        <Card image_path={aboutImage2} direction={'reverse'} title={"Focus"} description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, esse."/>
       </section>
 
       <aside className="">
