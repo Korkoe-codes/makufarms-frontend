f477004f809159e02445a84fd6af048e783af2ec
Korkoe27 <dumashiekorkoe@gmail.com>
2025-01-30 13:26:22 +0000

    fix(ui): fixed card padding for small

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import {aboutImage2} from "../assets/index"
+import {aboutImage, aboutImage2} from "../assets/index"
 import { Card } from "./Card"
 const About = () => {
   return (
@@ -9,11 +9,11 @@ const About = () => {
           <p className="font-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia explicabo, id fugit maxime reiciendis assumenda in accusamus rem minima? Mollitia sapiente, natus illo ut repudiandae sunt ex nemo nihil culpa?</p>
         </div>
 
+        <Card image_path={aboutImage} direction={'reverse'} title={"Focus"} description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, esse."/>
         <Card image_path={aboutImage2} direction={'reverse'} title={"Focus"} description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, esse."/>
-        <Card image_path={aboutImage2} direction={'reverse'} title={"Focus"} description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, esse."/>
-        <Card image_path={aboutImage2} direction={'normal'} title={"Focus"} description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, esse."/>
+        <Card image_path={aboutImage} direction={'normal'} title={"Focus"} description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, esse."/>
         <Card image_path={aboutImage2} title={"Focus"} direction={'normal'} description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, esse."/>
-        <Card image_path={aboutImage2} direction={'reverse'} title={"Focus"} description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, esse."/>
+        <Card image_path={aboutImage} direction={'reverse'} title={"Focus"} description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, esse."/>
       </section>
 
       <aside className="">
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,15 @@
 import PropTypes from 'prop-types';
 export const Card = ({ image_path, title, description,direction }) => {
 
-    const isReverse = direction === 'reverse' ? 'flex-row-reverse lg:flex-row flex text-right w-full lg:w-1/3 gap-2 shadow-sm bg-teal-50 rounded-lg p-4' : 'flex-row lg:flex-row-reserve flex w-full lg:w-1/3 gap-2 shadow-sm bg-teal-50 rounded-lg p-4';
+    const isReverse = direction === 'reverse' ? 'lg:flex-row-reverse lg:flex-row flex-col flex text-right w-full items-center lg:w-1/3 gap-2 shadow-sm bg-teal-50 rounded-lg p-10' : 'lg:flex-row flex-col flex w-full lg:w-1/3 gap-2 items-center shadow-sm bg-teal-50 rounded-lg p-10';
 
 
   return (
     <div className={isReverse}>
-      <div className="w-1/2">
+      <div className="lg:w-1/2">
         <img src={image_path} alt="" className="rounded-lg" />
       </div>
-      <div className="flex flex-col w-1/2  lg:items-center justify-center gap-2 lg:gap-4">
+      <div className="flex flex-col lg:w-1/2  lg:items-center text-left justify-center gap-2 lg:gap-4">
         <h1 className="font-semibold text-lg">{title}</h1>
         <p className="text-sm lg:text-base font-normal lg:text-center">{description}</p>
       </div>
