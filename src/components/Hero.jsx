184b2682973374b832a439b7e9dfbd5b110620b7
Korkoe27 <dumashiekorkoe@gmail.com>
2025-03-20 08:22:06 +0000

    work on hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
 
 
       <div  className="absolute w-full p-y-10 flex flex-col">
-      <div className="2xl:w-3/4 leading-5 flex h-screen flex-col top-10  w-full">
+      <div className="2xl:w-1/2 leading-5 flex h-screen flex-col top-10  w-full">
         <h4 className="text-3xl lg:text-2xl xl:text-6xl 2xl:text-8xl font-bold text-[#E6E6E9]">
         Empowering Communities Through Sustainable Agriculture
           </h4>
